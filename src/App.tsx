05e174a3d51fd47811c6e14cd1baaa2b595aab26
Abdelalime Bouassal <AbdelalimeBouassal@my.uopeople.edu>
2026-03-14 10:52:04 +0000

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,5 @@
-import { Navigation } from './components/Navigation';
-import { Hero } from './components/Hero';
-import { ProblemAgitate } from './components/ProblemAgitate';
-import { ProcessSteps } from './components/ProcessSteps';
-import { Comparison } from './components/Comparison';
-import { Pricing } from './components/Pricing';
-import { Founder } from './components/Founder';
-import { FinalCTA } from './components/FinalCTA';
-import { Footer } from './components/Footer';
-
 function App() {
-  return (
-    <div className="min-h-screen bg-brand-background font-sans text-brand-nearBlack selection:bg-brand-accent/30">
-      <Navigation />
-      
-      <main>
-        <Hero />
-        <ProblemAgitate />
-        <ProcessSteps />
-        <Comparison />
-        <Pricing />
-        <Founder />
-        <FinalCTA />
-      </main>
-
-      <Footer />
-    </div>
-  );
+  return <h1>Hello, World!</h1>;
 }
 
 export default App;
